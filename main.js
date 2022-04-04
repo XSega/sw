45564edab58c73889101fc7f4c734ef7bdc6569a
Sergey Ilyushin <s.ilyushin@corp.mail.ru>
2022-04-04 18:12:18 +0300

    direct

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
 var myButton = document.querySelector('button');
-
 myButton.onclick = function() {
-    // createGist({
-    //   description: 'Fetch API Post example',
-    //   public: true
-    // })
     fetchGithub();
 }
 
-registerSW();
+
+
+
+function register() {
+  window.addEventListener('load', () => {
+    registerValidSW();
+  });
+}
+
+function registerValidSW() {
+	navigator.serviceWorker
+		.register('sw.js')
+		.then((registration) => {
+			if (navigator.vendor === 'Apple Computer, Inc.') {
+				sendMessage('Safari!!!!');
+			}
+
+			registration.onupdatefound = () => {
+				const installingWorker = registration.installing;
+				if (installingWorker == null) {
+					return;
+				}
+				installingWorker.onstatechange = () => {
+					if (installingWorker.state === 'installed') {
+						if (navigator.serviceWorker.controller) {
+							// At this point, the updated precached content has been fetched,
+							// but the previous service worker will still serve the older
+							// content until all client tabs are closed.
+							sendMessage(
+								'New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+							);
+						} else {
+							// At this point, everything has been precached.
+							// It's the perfect time to display a
+							// "Content is cached for offline use." message.
+							sendMessage('Content is cached for offline use.');
+						}
+					}
+				};
+			};
+		})
+		.catch((error) => {
+			sendMessage('Error during service worker registration:', error);
+		});
+}
 
 function registerSW() {
     // Проверка того, что наш браузер поддерживает Service Worker API.
     navigator.serviceWorker.register('sw.js').then(function(registration) {
         sendMessage("ServiceWorker registration successful with scope: " + registration.scope);
+
+        
     }, function(err) {
         sendMessage("ServiceWorker registration failed: " +  err);
     });
@@ -21,10 +62,10 @@ function registerSW() {
 
 function fetchGithub() {
     console.log('Posting request to GitHub API...');
-    fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits').then(function(response) {
+    fetch('super-chashkin://api.github.com/repos/javascript-tutorial').then(function(response) {
         return response.json();
     }).then(function(data) {
-        sendMessage("Created Gist:" + data.html_url);
+        sendMessage("fetchGithub:" + data);
     });
 }
 
@@ -36,6 +77,11 @@ function createGist(opts) {
   }).then(function(response) {
     return response.json();
   }).then(function(data) {
-    sendMessage("Created Gist:" + data.html_url);
+    sendMessage("Created Gist:" + data);
   });
+}
+
+function sendMessage(message) {
+  console.log(message);
+  window.webkit.messageHandlers.notifications.postMessage(message);
 }
--- sw.js
+++ sw.js
@@ -1,19 +1,32 @@
 
 self.addEventListener('install', (event) => {
-    sendMessage('Установлен');
+    console.log('install');
 });
 
 self.addEventListener('activate', (event) => {
-    sendMessage('Активирован');
+    console.log('activate');
 });
 
 self.addEventListener('fetch', (event) => {
-    sendMessage("Происходит запрос на сервер" + event.request.body);
+    console.log("fetch " + event.request.body);
+
+    // request is event.request sent by browser here 
+    var request = event.request
+    
+    // разбираем URL
+    const requestURL = new URL(event.request.url)
+
+    // обрабатываем запросы к определенному хосту особым образом
+    if (requestURL.hostname === 'api.github.com') {
+        var req = new Request(request.url + "/en.javascript.info/commits", {
+            method: "get",
+            headers: request.headers,
+            mode: request.mode, // need to set this properly
+            credentials: request.credentials,
+            redirect: 'manual'   // let browser handle redirects
+        });
+        event.respondWith(fetch(req))
+        return
+    }
     event.respondWith(fetch(event.request))
 });
-
-function sendMessage(message) {
-    console.log(message);
-    window.webkit.messageHandlers.notifications.postMessage(message);
-}
-
