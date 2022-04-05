97c491e4291754a0ecb284bbb3378c3464393251
Sergey Ilyushin <s.ilyushin@corp.mail.ru>
2022-04-05 22:11:23 +0300

    html

--- main.js
+++ main.js
@@ -4,12 +4,20 @@ myButton.onclick = function() {
 }
 
 function fetchGithub() {
-    sendMessage('Posting request to GitHub API...');
-    fetch('super-chashkin://api.github.com/repos/javascript-tutorial/en.javascript.info/commits').then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        sendMessage("fetchGithub:" + data);
-    });
+    // sendMessage('Posting request to GitHub API...');
+    // fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits').then(function(response) {
+    //     return response.json();
+    // }).then(function(data) {
+    //     sendMessage("fetchGithub:" + data);
+    // });
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits', true);
+    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.onload = function () {
+        console.log(xhr.responseURL); // http://example.com/test
+    };
+    xhr.send(null);
 }
 
 function sendMessage(message) {
